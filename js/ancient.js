522384035ef2e9b9bbf7c3321348e0ef5fd4911c
Slava Ivanov <drumtrack@ya.ru>
2022-08-22 19:52:31 +0300

    init: start codejam

---
+++ js/ancient.js
@@ -0,0 +1,93 @@
+const ancientsData = [
+    {
+        id: 'azathoth',
+        name: 'Azathoth',
+        // cardFace: Ancients.azathoth,
+        firstStage: {
+            greenCards: 1,
+            blueCards: 1,
+            brownCards: 2,
+        },
+        secondStage: {
+            greenCards: 2,
+            blueCards: 1,
+            brownCards: 3,
+        },
+        thirdStage: {
+            greenCards: 2,
+            blueCards: 0,
+            brownCards: 4,
+        },
+    },
+    {
+        id: 'cthulhu',
+        name: 'Cthulhu',
+        // cardFace: Ancients.cthulhu,
+        firstStage: {
+            greenCards: 0,
+            blueCards: 2,
+            brownCards: 2,
+        },
+        secondStage: {
+            greenCards: 1,
+            blueCards: 0,
+            brownCards: 3,
+        },
+        thirdStage: {
+            greenCards: 3,
+            blueCards: 0,
+            brownCards: 4,
+        },
+    },
+    {
+        id: 'iogSothoth',
+        name: 'Yog-Sothoth',
+        // cardFace: Ancients.iogSothoth,
+        firstStage: {
+            greenCards: 0,
+            blueCards: 1,
+            brownCards: 2,
+        },
+        secondStage: {
+            greenCards: 2,
+            blueCards: 1,
+            brownCards: 3,
+        },
+        thirdStage: {
+            greenCards: 3,
+            blueCards: 0,
+            brownCards: 4,
+        },
+    },
+    {
+        id: 'shubNiggurath',
+        name: 'Shub-Niggurath',
+        // cardFace: Ancients.shubNiggurath,
+        firstStage: {
+            greenCards: 1,
+            blueCards: 1,
+            brownCards: 2,
+        },
+        secondStage: {
+            greenCards: 3,
+            blueCards: 1,
+            brownCards: 2,
+        },
+        thirdStage: {
+            greenCards: 2,
+            blueCards: 0,
+            brownCards: 4,
+        },
+    },
+];
+
+const ancientsBlock = document.querySelector(".ancients");
+ancientsBlock.addEventListener("click", setAncient);
+
+function setAncient(event) {
+    const ancient = ancientsData.find(e => e.id === event.target.dataset.ancient);
+    console.log(ancient);
+    return ancient;
+}
+
+export {setAncient};
---
+++ js/app.js
@@ -0,0 +1,1 @@
+import { setAncient } from "./ancient.js";
