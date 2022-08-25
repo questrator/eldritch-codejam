29137a78e94da10ec18065da7d174967073d87b3
Slava Ivanov <drumtrack@ya.ru>
2022-08-25 19:34:22 +0300

    upd: deck, logic

--- js-alt/app.js
+++ js-alt/app.js
@@ -1,64 +1,84 @@
 import * as config from "./config.js";
-import "./ancient.js";
-import "./difficulty.js";
+import { getRandomN, shuffle } from "./randomizer.js";
 
 let ancient = null;
 let difficulty = null;
+let deck = null;
 
-function setAncient(data) {
-    ancient = data;
-    console.log("ANCIENT", ancient);
-}
+const difficultiesBlock = document.querySelector(".difficulties");
+difficultiesBlock.addEventListener("click", setDifficulty);
+const ancientsBlock = document.querySelector(".ancients");
+ancientsBlock.addEventListener("click", setAncient);
 
-function setDifficulty(data) {
-    difficulty = data;
+function setDifficulty(event) {
+    difficulty = config.difficulties[event.target.dataset.level];
     console.log("DIFFICULTY", difficulty);
 }
 
-/* START */
+function setAncient(event) {
+    ancient = event.target.dataset.ancient === "random"
+    ? Object.values(config.ancients)[getRandomN(0, Object.keys(config.ancients).length - 1)]
+    : config.ancients[event.target.dataset.ancient];
+    console.log("ANCIENT", ancient);
+}
 
 const start = document.querySelector(".go");
-start.addEventListener("click", getSpread);
+start.addEventListener("click", getDeck);
 const colors = Object.keys(config.cards);
+const cardTypes = Object.keys(config.cards[colors[0]]);
 
-function getSpread() {
+function getDeck() {
     if (!ancient || !difficulty) return;
-
+    const deck = JSON.parse(JSON.stringify(config.cards));
     const needCards = {};
-    const allCards = {};
-    const removeCards = {};
     colors.forEach(e => needCards[e] = ancient.pack[e].reduce((r, e) => r + e, 0));
-    colors.forEach(e => allCards[e] = Object.values(config.cards[e]).flat().length);
-    colors.forEach(e => removeCards[e] = Object.values(config.cards[e]).flat().length - ancient.pack[e].reduce((r, e) => r + e, 0));
-    console.log("need ", needCards);
-    console.log("all ", allCards);
-    console.log("remove ", removeCards);
-
-    const spread = Object.assign({}, config.cards);
-
+    
     if (difficulty.forbidden) {        
-        for (let key in spread) {
-            delete spread[key][difficulty.forbidden];
+        for (let color in deck) {
+            delete deck[color][difficulty.forbidden];
         }
     }
     
-    if (difficulty.required) {
-        console.log("required:", difficulty.required);
-    }
-    
-    if (difficulty.remaining) {
-        console.log("remaining:", difficulty.remaining);
+    if (difficulty.required && difficulty.remaining) {
+        const remainingN = {};
+        colors.forEach(color => remainingN[color] = needCards[color] - deck[color][difficulty.required].length);
         
+        for (let color in remainingN) {
+            if (remainingN[color] <= 0) {
+                delete deck[color][difficulty.remaining];
+                deck[color][difficulty.required] = shuffle(deck[color][difficulty.required]).slice(0, remainingN[color] === 0 ? deck[color][difficulty.required].length : remainingN[color]);
+            }
+            if (remainingN[color] > 0) {
+                deck[color][difficulty.remaining] = shuffle(deck[color][difficulty.remaining]).slice(0, remainingN[color]);
+            }
+        }
+        colors.forEach(color => deck[color] = shuffle(Object.values(deck[color]).flat()));
     }
-    
-    console.log("ANCIENT PACK", ancient.pack);
-    console.log("SPREAD", spread);
-    return spread;
+
+    if (!difficulty.required && !difficulty.remaining) {
+        colors.forEach(color => deck[color] = shuffle(Object.values(deck[color]).flat()).slice(0, needCards[color]));
+    }
+
+    showCards(deck);
+    return deck;
 }
 
-export {
-    ancient,
-    difficulty,
-    setAncient,
-    setDifficulty
-};
+function showCards(deck) {
+    console.log(config.cards);
+    console.log(deck);
+
+    colors.forEach(color => {
+        for (let i = 0; i < deck[color].length; i++) {
+            for (let diff in config.cards[color]) {
+                if (config.cards[color][diff].includes(deck[color][i])) {
+                    console.log(deck[color][i], color, diff);
+                    
+                }
+            }
+        }
+    });
+}
+
+function getSpread(deck) {
+    const spread = 
+}
